7b35bc6dd0dd53052453133dc2300686308ae743
Elliot Nash <elliotnash@gmail.com>
2024-07-09 11:52:15 -0700

    Update stdlib version

--- depends.ts
+++ depends.ts
@@ -1,5 +1,5 @@
 export {
   assertEquals,
   assertThrows,
-} from "https://deno.land/std@0.55.0/testing/asserts.ts";
+} from "jsr:@std/assert@1.0.0";
 export { sprintf } from "https://deno.land/std@0.55.0/fmt/printf.ts";
--- example.ts
+++ example.ts
@@ -1,4 +1,4 @@
-import { Uint32 } from "https://deno.land/x/typed_numeric/mod.ts";
+import { Uint32 } from "./mod.ts";
 
 const a = new Uint32(200n);
 const b = new Uint32(100n);
@@ -10,8 +10,8 @@ console.log(a.mul(b).value()); // 20000n
 
 const c = new Uint32(0x1234_5678n);
 
-console.log(c.toBeBytes().toTypedArray()); // [18, 52, 86, 120]([0x12, 0x34, 0x56, 0x78])
-console.log(c.toLeBytes().toTypedArray()); // [120, 86, 52, 18]([0x78, 0x56, 0x34, 0x12])
+console.log(c.toBeBytes()); // [18, 52, 86, 120]([0x12, 0x34, 0x56, 0x78])
+console.log(c.toLeBytes()); // [120, 86, 52, 18]([0x78, 0x56, 0x34, 0x12])
 
 const d = Uint32.fromBeBytes(
   new Uint8Array([0x12, 0x34, 0x56, 0x78]),
